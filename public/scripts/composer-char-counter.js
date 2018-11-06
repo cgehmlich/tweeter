73c34db8cf182c51758a4369e43605a47fbc3225
cgehmlich <cgehmlich@gmail.com>
2018-11-06 23:38:59 +0000

    showing builtin tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('#txtid').on('keyup', function() {
+  var char = (this.value.length+1)
+  remainder = 140 - char;
+  $('span.counter').text(remainder);
+  if (remainder < 0) {
+    document.getElementById('counter').style.color = '#ff0000'
+  }
+  else {document.getElementById('counter').style.color = 'initial'}
+  })
+  });
